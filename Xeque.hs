88f14f5bc58e53d1e3f94ed3f585b31076da3df1
David Pessoa <104323068+david-pessoa@users.noreply.github.com>
2024-03-21 17:02:09 -0300

    Update Xeque.hs

--- Xeque.hs
+++ Xeque.hs
@@ -6,23 +6,22 @@ member :: Char -> [Char] -> Bool
 member _ [] = False
 member y (a:x) = (y == a) || member y x
 
+addVazioInt :: Int -> [Char]
+addVazioInt num
+  | num <= 0 = []
+  | otherwise replicate num ' '
+
+addVazio :: Char -> [Char]
+addVazio num = addVazioInt(ord num)
+
 geraVetor :: Char -> [Char] -> [Char]
 geraVetor _ [] = []
 geraVetor (a:x)
   | (member a letras == True) = [a] ++ geraVetor x
-  | otherwise = addVazio a ++ geraVetor
-
-
-
-
-
-
-
-
+  | otherwise = addVazio a ++ geraVetor x
 
 genMatriz :: [String] -> [String]
-genMatriz (a:x)
-  |
+genMatriz (a:x) = geraVetor(take a) a ++ geraMatriz x
 
 
 
@@ -30,4 +29,4 @@ genMatriz (a:x)
 
 main :: IO ()
 main = do
-  putStrLn $ show (ord 'A')
+  putStrLn $ show (genMatriz ["tcbdrbct","pppppppp","8","8","8","8","PPPPPPPP","TCBDRBCT"])
